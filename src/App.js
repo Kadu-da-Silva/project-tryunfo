2343a828569c77f6e5ccf8552ef1a8bcaed8fb8c
Carlos Eduardo <carlao0010@hotmail.com>
2023-04-07 16:52:57 -0300

    Corrigindo Lint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
 
+const INITIAL_STATE = {
+  cardName: '',
+  cardDescription: '',
+  cardAttr1: '',
+  cardAttr2: '',
+  cardAttr3: '',
+  cardImage: '',
+  cardRare: '',
+  cardTrunfo: false,
+  hasTrunfo: false,
+  isSaveButtonDisabled: true,
+};
+
 class App extends React.Component {
   constructor() {
     super();
@@ -9,18 +22,7 @@ class App extends React.Component {
     this.onInputChange = this.onInputChange.bind(this);
     // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
 
-    this.state = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
-      cardImage: '',
-      cardRare: '',
-      cardTrunfo: false,
-      hasTrunfo: false,
-      isSaveButtonDisabled: true,
-    };
+    this.state = INITIAL_STATE;
   }
 
   onInputChange({ target }) {
